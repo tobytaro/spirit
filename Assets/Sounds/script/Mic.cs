4647d7391e27d7f17dfe6586b4d4e6db0197fffb
qian2 <yanlin.qian@student.tut.fi>
2016-08-03 16:55:44 +0300

    add measurement of human sound

---
+++ Assets/Sounds/script/Mic.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mic : MonoBehaviour {
+
+
+	public string devicename="Built-in Microphone";
+	// Use this for initialization
+	//void Start () {
+	
+	//}
+	
+	// Update is called once per frame
+	public void Update () {
+		if (Microphone.IsRecording (devicename)) {
+			StopRecording ();
+		} else {
+			StartRecording ();
+		}
+	}
+	void StartRecording (){
+		GetComponent<AudioSource> ().clip=Microphone.Start (devicename,true,30,44100);
+		while (!(Microphone.GetPosition (null) > 0)) {
+		}
+		GetComponent<AudioSource> ().Play ();
+
+		//Debug.Log (GetComponent<AudioSource> ().clip.samples);
+		//Debug.Log (GetComponent<AudioSource> ().clip.channels);
+		Debug.Log(Microphone.GetPosition(null));
+
+		//measure the volume
+		int length_data=GetComponent<AudioSource> ().clip.samples* GetComponent<AudioSource> ().clip.channels;
+		float[] data = new float[length_data];
+		GetComponent<AudioSource> ().clip.GetData (data, 0);
+		//take the median of the recorded samples
+		float sum=0;
+		foreach (float f in data)
+		{
+			sum = sum + Mathf.Abs (f);
+		}
+//		float Volume = (float)s[735-1];
+		float Volume=sum;
+		Debug.Log (Volume);
+	}
+	void StopRecording(){
+		Microphone.End (devicename);
+		GetComponent<AudioSource> ().Stop ();
+	}
+
+}
---
+++ Assets/Sounds/script/soundmeasuring.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class soundmeasuring : MonoBehaviour {
+
+	Text txt;
+	public void ClickTest(){
+		
+		txt=GetComponentInChildren<Text>();
+		foreach (string device in Microphone.devices) {
+			Debug.Log("Name: " + device);
+			//		txt.text = "yanlin";
+			txt.text="Name:"+device;
+		}
+			
+
+	}
+}
