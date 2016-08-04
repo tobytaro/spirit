247e4f25e32c95015e6952d537f1a6d2b3ececa1
qian2 <yanlin.qian@student.tut.fi>
2016-08-04 14:55:30 +0300

    add slider bar, and real time sound visualization

--- Assets/Sounds/script/Mic.cs
+++ Assets/Sounds/script/Mic.cs
@@ -1,36 +1,39 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Mic : MonoBehaviour {
 
 
 	public string devicename="Built-in Microphone";
+	public float sensitivity=100;
+	public float loudness=0;
+	public float threshold = 1.0f;
+
+	//slider
+	public Slider slider;
+
 	// Use this for initialization
-	//void Start () {
-	
-	//}
-	
-	// Update is called once per frame
-	public void Update () {
+	public void Start () {
 		if (Microphone.IsRecording (devicename)) {
 			StopRecording ();
 		} else {
 			StartRecording ();
 		}
+		//slider = GetComponent<Slider> ();
 	}
-	void StartRecording (){
-		GetComponent<AudioSource> ().clip=Microphone.Start (devicename,true,30,44100);
-		while (!(Microphone.GetPosition (null) > 0)) {
-		}
-		GetComponent<AudioSource> ().Play ();
+	
+	// Update is called once per frame
+	public void Update () {
+		loudness = GetAveragedVolume ();
+		Debug.Log ("loudness:"+loudness);
+		//slider.value = loudness;
+		slider.value=Mathf.MoveTowards(slider.value,loudness,0.01f);
+	}
 
-		//Debug.Log (GetComponent<AudioSource> ().clip.samples);
-		//Debug.Log (GetComponent<AudioSource> ().clip.channels);
-		Debug.Log(Microphone.GetPosition(null));
-
-		//measure the volume
-		int length_data=GetComponent<AudioSource> ().clip.samples* GetComponent<AudioSource> ().clip.channels;
-		float[] data = new float[length_data];
+	float GetAveragedVolume(){
+		
+		float[] data = new float[256];
 		GetComponent<AudioSource> ().clip.GetData (data, 0);
 		//take the median of the recorded samples
 		float sum=0;
@@ -38,9 +41,22 @@ public class Mic : MonoBehaviour {
 		{
 			sum = sum + Mathf.Abs (f);
 		}
-//		float Volume = (float)s[735-1];
-		float Volume=sum;
-		Debug.Log (Volume);
+		return sum/256*sensitivity;
+
+	}
+	void StartRecording (){
+		GetComponent<AudioSource> ().clip=Microphone.Start (devicename,true,1,44100);
+		while (!(Microphone.GetPosition (null) > 0)) {
+		}
+		//GetComponent<AudioSource> ().Play ();
+
+		//Debug.Log (GetComponent<AudioSource> ().clip.samples);
+		//Debug.Log (GetComponent<AudioSource> ().clip.channels);
+		//Debug.Log(Microphone.GetPosition(null));
+
+		//measure the volume
+		int length_data=GetComponent<AudioSource> ().clip.samples* GetComponent<AudioSource> ().clip.channels;
+
 	}
 	void StopRecording(){
 		Microphone.End (devicename);
